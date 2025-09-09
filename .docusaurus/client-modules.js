fb5540f2898fe6816dba768f86b37422b8c5b0ac
Fabrice <fab.hatege15@gmail.com>
2025-09-09 20:29:15 +0200

    update

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/home/fabrice/rendu/documentation/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/fabrice/rendu/documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/home/fabrice/rendu/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/fabrice/rendu/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/fabrice/rendu/documentation/src/css/custom.css"),
+];
